caf99934306ed2855e96e6aa4d77574f8c507315
Igor Morenets <igor.morenec@gmail.com>
2019-03-04 15:01:15 +0200

    Implemented landing activity layout. Added success color.

--- app/src/main/java/com/ukma/mylibrary/MainActivity.java
+++ app/src/main/java/com/ukma/mylibrary/MainActivity.java
@@ -2,14 +2,20 @@ package com.ukma.mylibrary;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public
 class MainActivity extends AppCompatActivity {
 
     @Override
     protected
-    void onCreate(Bundle savedInstanceState) {
+    void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public
+    void onContinueClicked(final View view) {
+
     }
 }
