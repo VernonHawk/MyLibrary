df0d4486876aa665172e8c8ff316f53f24b61455
Igor Morenets <igor.morenec@gmail.com>
2019-03-04 16:05:25 +0200

    Added empty Sign In Activity. Added Role enum. Implemented starting it from the MainActivity and passing the chosen Role.

---
+++ app/src/main/java/com/ukma/mylibrary/SignInActivity.java
@@ -0,0 +1,16 @@
+package com.ukma.mylibrary;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public
+class SignInActivity extends AppCompatActivity {
+
+    @Override protected
+    void onCreate(final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+
+        // use passed role
+    }
+}
