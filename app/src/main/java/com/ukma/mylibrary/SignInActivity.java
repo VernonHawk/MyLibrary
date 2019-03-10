1c82f36cd3648249aaf31e7dc60f29dee6fb30eb
Igor Morenets <igor.morenec@gmail.com>
2019-03-10 20:10:14 +0200

    Implemented Role-dependant to_sign_up block display. Now formatting Sign In headers with Role name. Changed some layouts ids to be less implementation dependant. Removed misc.xml from git repo.

--- app/src/main/java/com/ukma/mylibrary/SignInActivity.java
+++ app/src/main/java/com/ukma/mylibrary/SignInActivity.java
@@ -4,16 +4,25 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.TextView;
 
 public
 class SignInActivity extends AppCompatActivity {
+
+    private Role userRole;
 
     @Override protected
     void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
 
-        // use passed role
+        userRole = (Role) getIntent().getSerializableExtra(MainActivity.EXTRA_MESSAGE);
+
+        formatLabelWithRole(R.id.hello_label, R.string.hello_user_role);
+        formatLabelWithRole(R.id.label_not_user_role, R.string.not_a_user_role);
+
+        if (userRole != Role.Reader)
+            findViewById(R.id.to_sign_up_layout).setVisibility(View.GONE);
     }
 
     public
@@ -24,5 +33,12 @@ class SignInActivity extends AppCompatActivity {
     public
     void onToLanding(final View view) {
         finish();
+    }
+
+    private
+    void formatLabelWithRole(final int viewId, final int stringId) {
+        ((TextView) findViewById(viewId)).setText(
+            String.format(getString(stringId), userRole.name())
+        );
     }
 }
