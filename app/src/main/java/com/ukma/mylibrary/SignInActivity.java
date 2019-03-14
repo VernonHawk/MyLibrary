37e6c8b148aa8fe6859905868cf9b59de141e448
Annabell <Ts.Nyutka@gmail.com>
2019-03-14 23:33:22 +0200

    Do refactoring

--- app/src/main/java/com/ukma/mylibrary/SignInActivity.java
+++ app/src/main/java/com/ukma/mylibrary/SignInActivity.java
@@ -15,8 +15,8 @@ class SignInActivity extends AppCompatActivity {
 
     private EditText mPhoneEditText;
 
-    @Override protected
-    void onCreate(final Bundle savedInstanceState) {
+    @Override
+    protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
 
@@ -26,15 +26,15 @@ class SignInActivity extends AppCompatActivity {
             findViewById(R.id.to_sign_up_layout).setVisibility(View.GONE);
 
         final boolean startsWithVowel =
-            "aeiou".indexOf(Character.toLowerCase(mUserRole.name().charAt(0))) != -1;
+                "aeiou".indexOf(Character.toLowerCase(mUserRole.name().charAt(0))) != -1;
 
         final TextView notUserRole = findViewById(R.id.label_not_user_role);
 
         notUserRole.setPaintFlags(notUserRole.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
 
         formatLabelWithRole(
-            notUserRole.getId(),
-            startsWithVowel ? R.string.not_an_user_role : R.string.not_a_user_role
+                notUserRole.getId(),
+                startsWithVowel ? R.string.not_an_user_role : R.string.not_a_user_role
         );
         formatLabelWithRole(R.id.hello_label, R.string.hello_user_role);
 
@@ -42,25 +42,21 @@ class SignInActivity extends AppCompatActivity {
         PhoneNumberHelper.addFormatting(mPhoneEditText = findViewById(R.id.input_phone));
     }
 
-    public
-    void onToSignUp(final View view) {
+    public void onToSignUp(final View view) {
         startActivity(new Intent(SignInActivity.this, SignUpActivity.class)); //ReaderActivity.class);
     }
 
-    public
-    void onToLanding(final View view) {
+    public void onToLanding(final View view) {
         finish();
     }
 
-    private
-    void formatLabelWithRole(final int viewId, final int stringId) {
+    private void formatLabelWithRole(final int viewId, final int stringId) {
         ((TextView) findViewById(viewId)).setText(
-            String.format(getString(stringId), mUserRole.name())
+                String.format(getString(stringId), mUserRole.name())
         );
     }
 
-    public
-    void onSignIn(final View view) {
+    public void onSignIn(final View view) {
         final String phoneNumber = PhoneNumberHelper.normalize(mPhoneEditText.getText());
 
         // ...
